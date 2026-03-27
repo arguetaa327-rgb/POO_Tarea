d377b069c82db2cb9439e8cf04ff25f908fc8ced
Alondra Argueta <arguetaa327@gmail.com>
2026-03-27 12:44:05 -0600

    se realizo el ejercicio1 de Persona

--- src/Encapsulasion/Persona.java
+++ src/Encapsulasion/Persona.java
@@ -1,5 +1,21 @@
-package Encapsulasion;
+package Encapsulacion;
 
 public class Persona {
 
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Persona registrada:");
+        System.out.println("Nombre completo: " + nombre + " " + apellido);
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("------");
+    }
 }
