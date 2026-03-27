a02b322c01bd30693d924cb404504bca714c9a3d
Alondra Argueta <arguetaa327@gmail.com>
2026-03-27 12:59:23 -0600

    se realizo el ejercicio  de libro

--- src/Encapsulacion/Libro.java
+++ src/Encapsulacion/Libro.java
@@ -2,4 +2,24 @@ package Encapsulacion;
 
 public class Libro {
 
+    private String titulo;
+    private String autor;
+    private int paginas;
+
+    public Libro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+
+    public boolean esLargo() {
+        return paginas > 300;
+    }
+
+    public void mostrarLibro() {
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Paginas: " + paginas);
+        System.out.println("Es largo: " + esLargo());
+    }
 }
