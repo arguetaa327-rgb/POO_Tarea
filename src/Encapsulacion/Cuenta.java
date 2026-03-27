96da94720936a8165ac8f8665c0843325b7df755
Alondra Argueta <arguetaa327@gmail.com>
2026-03-27 12:57:40 -0600

    Se realizo el ejercicio de cuenta

--- src/Encapsulacion/Cuenta.java
+++ src/Encapsulacion/Cuenta.java
@@ -2,4 +2,30 @@ package Encapsulacion;
 
 public class Cuenta {
 
+    private String titular;
+    private double saldo;
+
+    public Cuenta(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+        }
+    }
+
+    public void retirar(double monto) {
+        if (monto <= saldo) {
+            saldo -= monto;
+        } else {
+            System.out.println("No hay suficiente saldo");
+        }
+    }
+
+    public void verSaldo() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: Q" + saldo);
+    }
 }
